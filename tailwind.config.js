74859a08b21ff529d0e2e5bd68b9337f2d7b2aec
Buor <buor0@mail.ru>
2022-05-10 21:22:02 +0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'bookman': ['Bookman Old Style', 'serif'],
+        'cuprum': ['Cuprum', 'serif'],
+        'freestyle': ['Freestyle Script', 'serif']
+      }
+    },
+  },
+  plugins: [],
+}
