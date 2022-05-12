b002655a23186cca3563e6180a6855b4316a533a
Alwafor <alwafor1@gmail.com>
2022-05-12 16:37:11 +0300

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './App.module.scss';
 import {Navbar} from "./components/navbar/Navbar";
 import {Sidebar} from "./components/layout/sidebar/Sidebar";
+import {routes} from './contants/routes';
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
     return (
@@ -9,7 +11,9 @@ function App() {
             <Navbar/>
                 <div className={'flex'}>
                     <Sidebar/>
-
+                    <Routes>
+                        {routes.map(route => <Route key={route.path} element={route.component} path={route.path}/>)}
+                    </Routes>
                 </div>
 
         </div>
--- src/pages/create-creature-page/CreateCreaturePage.tsx
+++ src/pages/create-creature-page/CreateCreaturePage.tsx
@@ -6,6 +6,6 @@ interface IProps {
 
 export const CreateCreaturePage: React.FC<IProps> = () => {
     return <div>
-
+        Create creature page
     </div>
 };
