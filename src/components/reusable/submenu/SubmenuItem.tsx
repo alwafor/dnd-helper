e5fd055e7404870fe1dab77a9f38aab516bc2d98
Alwafor <alwafor1@gmail.com>
2022-05-12 20:24:37 +0300

    Added 'activity' submenu item

--- src/components/reusable/submenu/SubmenuItem.tsx
+++ src/components/reusable/submenu/SubmenuItem.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import styles from './Submenu.module.scss'
+import {ISubmenuItems} from '../../../types/submenuTypes';
 
-interface IProps {
-    name: string
-    img?: string
-    onClick: Function
-}
+export const SubmenuItem: React.FC<ISubmenuItems> = ({name, icon, onClick, isActive}) => {
 
-export const SubmenuItem: React.FC<IProps> = ({name, img, onClick}) => {
-    return <button className={styles.submenuItem} onClick={() => onClick()}>
-        <img className={styles.submenuImage} src={img} alt={name}/>
+    return <button className={styles.submenuItem + (isActive ? ' ' + styles.active : '')} onClick={() => onClick()}>
+        <img className={styles.submenuImage} src={icon} alt={name}/>
         <div className={styles.submenuName}>{name}</div>
     </button>
 };
