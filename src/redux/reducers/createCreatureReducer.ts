3af616bce2755057d50a1308b58f884377ff2a64
Alwafor <alwafor1@gmail.com>
2022-05-20 13:51:25 +0300

    Create base redux store and reducer

---
+++ src/redux/reducers/createCreatureReducer.ts
@@ -0,0 +1,18 @@
+import {ICreatureData} from '../../types/creatureTypes';
+import {createCreatureDefaultValues} from '../../contants/hookFormConstants';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+const initialState = {
+    formValues: createCreatureDefaultValues
+}
+
+export const createCreatureSlice = createSlice({
+    name: 'createCreature',
+    initialState,
+    reducers: {
+        changeFormValues: (state, action: PayloadAction<Partial<ICreatureData>>) => {
+            state.formValues = action.payload
+        }
+    }
+})
+
