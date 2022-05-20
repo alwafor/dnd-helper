c416e7de34fdd31120a01ef1fac660e7555094a4
Alwafor <alwafor1@gmail.com>
2022-05-20 16:10:45 +0300

    Create and styled head for bestiary page

--- src/pages/bestiary-page/BestiaryPage.tsx
+++ src/pages/bestiary-page/BestiaryPage.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import styles from './BestiaryPage.module.scss'
+import imgWolf from './../../assets/images/big/wolf.png'
+import {CreatureCard} from '../../components/pages/bestiary-page/creature-card/CreatureCard';
+import {useAppSelector} from '../../hooks/redux';
 
 export const BestiaryPage: React.FC = () => {
-    return <div>
-        Bestiary page
+
+    const allCreatures = useAppSelector(state => state.creatures.creatures)
+
+    return <div className={styles.bestiaryPage}>
+        <div className={styles.head}>
+            <img className={styles.headImg} src={imgWolf} alt="Bestiary"/>
+            <div className={styles.title}>Бестиарий</div>
+        </div>
+        <div className={styles.creaturesWrapper}>
+            <CreatureCard creatureData={allCreatures[0]}/>
+        </div>
     </div>
 };
