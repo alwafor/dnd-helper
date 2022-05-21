ce095e649c1ef991e5c884c4c6cb90e3bf0ae239
Alwafor <alwafor1@gmail.com>
2022-05-21 12:33:35 +0300

    Add two util creature functions

--- src/utils/creatureCalculation.ts
+++ src/utils/creatureCalculation.ts
@@ -1,3 +1,13 @@
+import {ICreatureData} from '../types/creatureTypes';
+
 export function statToModifier(stat: string) {
     return Math.floor((+stat - 10) / 2)
+}
+
+export function getCreatureStats({strength, agility, constitution, intelligence, wisdom, charisma}: ICreatureData) {
+    return [{name: 'Сил', value: strength},{name: 'Лов', value: agility},{name: 'Тел', value: constitution},{name: 'Инт', value: intelligence},{name: 'Мдр', value: wisdom},{name: 'Хар', value: charisma}]
+}
+
+export function formHeadDescStr({size, type, worldview}: ICreatureData) {
+    return `${size[0].toUpperCase() + size.slice(1)}, ${type}, ${worldview}`
 }
