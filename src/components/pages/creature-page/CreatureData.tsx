a3b529980861d7c3dbc1ca038857ee002581e7de
Alwafor <alwafor1@gmail.com>
2022-05-22 21:00:45 +0300

    Add stats to creature data component
    Create short Stat component in
    creature data file

--- src/components/pages/creature-page/CreatureData.tsx
+++ src/components/pages/creature-page/CreatureData.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from './CreatureData.module.scss'
 import {ICreatureData} from '../../../types/creatureTypes';
-import {formHeadDescStr, formSpeedStr} from '../../../utils/creatureCalculation';
+import {formHeadDescStr, formSpeedStr, statToModifier} from '../../../utils/creatureCalculation';
 import {DataBlock} from './DataBlock';
+import {addPlusToPositive} from '../../../utils/stringUtils';
 
 interface IProps {
     creatureData: ICreatureData
@@ -25,5 +26,24 @@ export const CreatureData: React.FC<IProps> = ({creatureData}) => {
                 <p><span className={styles.red}>Скорость</span> {formSpeedStr(creatureData)}</p>
             </>
         </DataBlock>
+        <DataBlock>
+            <>
+                <span>
+                    <Stat name={'СИЛ'} stat={creatureData.strength}/>
+                    <Stat name={'ЛОВ'} stat={creatureData.agility}/>
+                    <Stat name={'ТЕЛ'} stat={creatureData.constitution}/>
+                    <Stat name={'ИНТ'} stat={creatureData.intelligence}/>
+                    <Stat name={'МДР'} stat={creatureData.wisdom}/>
+                    <Stat name={'ХАР'} stat={creatureData.charisma}/>
+                </span>
+            </>
+        </DataBlock>
     </div>
 };
+
+const Stat: React.FC<{stat: string, name: string}> = ({name, stat}) => {
+    return <>
+        <span className={styles.red}>{name} </span> <span
+        className={styles.mr}>{stat} ({addPlusToPositive(statToModifier(stat))})</span>
+    </>
+}
