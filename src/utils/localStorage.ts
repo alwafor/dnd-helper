ac99be90f936960a97ea233efc493cfa19a29c96
Alwafor <alwafor1@gmail.com>
2022-06-04 10:32:40 +0300

    Create LocalStorage api
    - Creatures data will be placed
    into localStorage
    - LocalStorage static methods
    implemented

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,30 @@
+import {ICreatureData} from '../types/creatureTypes'
+
+export class LocalStorage {
+    static getCreatures() {
+        const creatures = localStorage.getItem('creatures')
+
+        if(!creatures) return []
+
+        let parsedCreatures = []
+        try {
+            parsedCreatures = JSON.parse(creatures)
+        }
+        catch (e) {
+            console.error(`Error! Can parse creatures! Something is wrong with localStorage data!`)
+            LocalStorage.clearStorage()
+        }
+
+        return parsedCreatures as ICreatureData[]
+    }
+
+    static addCreature(creature: ICreatureData) {
+        const creatures = LocalStorage.getCreatures()
+        creatures.push(creature)
+        localStorage.setItem('creatures', JSON.stringify(creatures))
+    }
+
+    private static clearStorage() {
+        localStorage.clear()
+    }
+}
