3b1d6f19eeb3a896939b82d363f7b8e8ebf2eff5
Alwafor <alwafor1@gmail.com>
2022-06-04 12:47:19 +0300

    Add modifyCreature action to creatures reducer

--- src/redux/reducers/creaturesReducer.ts
+++ src/redux/reducers/creaturesReducer.ts
@@ -13,6 +13,17 @@ export const creaturesSlice = createSlice({
         addCreature(state, action: PayloadAction<ICreatureData>) {
             state.creatures.push(action.payload)
             LocalStorage.addCreature(action.payload)
+        },
+        modifyCreature(state, {payload: {creatureData, oldName}}: PayloadAction<{creatureData: ICreatureData, oldName: string}>) {
+            const creatureToModify = state.creatures.find(creature => creature.name === oldName)
+            if(!creatureToModify) throw new Error(`The creature to modify doesn't exists!`)
+
+            state.creatures = state.creatures.map(creature => {
+                if(creature === creatureToModify) return creatureData
+                return creature
+            })
         }
     }
 })
+
+export const {addCreature, modifyCreature} = creaturesSlice.actions
