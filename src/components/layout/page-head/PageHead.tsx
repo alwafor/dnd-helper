6583ec49e297d91265b707ce28de85d06cbf83d3
Alwafor <alwafor1@gmail.com>
2022-06-07 09:52:06 +0300

    Add style prop to PageHead

--- src/components/layout/page-head/PageHead.tsx
+++ src/components/layout/page-head/PageHead.tsx
@@ -3,12 +3,13 @@ import styles from './PageHead.module.css'
 
 interface IProps {
     img: string,
-    title: string
+    title: string,
+    style?: any
 }
 
-export const PageHead: React.FC<IProps> = ({img, title}) => {
+export const PageHead: React.FC<IProps> = ({img, title, style = {}}) => {
     return <div className={styles.head}>
-        <img className={styles.headImg} src={img} alt="image"/>
+        <img className={styles.headImg} style={{...style}} src={img} alt="image"/>
         <div className={styles.title}>{title}</div>
     </div>
 }
