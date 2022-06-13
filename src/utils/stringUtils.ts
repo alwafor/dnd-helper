a744c4aa498ff4f27431e42ac829ae515f570f2d
Alwafor <alwafor1@gmail.com>
2022-06-13 16:44:27 +0300

    Add util func to stringUtils

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,1 +1,11 @@
 export const addPlusToPositive = (n: number) => n > 0 ? '+' + n : String(n)
+
+export const removeSubstringFromStringWithComas = (str: string, substr: string) => {
+    let result = str.replace(new RegExp(`,?\\s*${substr}`, 'i'), '')
+    let resultSplit = result.split(/,\s*/)
+
+    if (resultSplit.length === 2 && resultSplit[0] === '')
+        result = result.replace(/,\s*/, '')
+
+    return result
+}
