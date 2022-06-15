ef02325b4a0ef456d28a240cbfbabd5956ea23fe
Alwafor <alwafor1@gmail.com>
2022-06-15 10:57:36 +0300

    Improve string util function
    - removeSubstringFromStringWithComas
    has been improved, code amount reduced,
    function became cleaner

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,11 +1,8 @@
 export const addPlusToPositive = (n: number) => n > 0 ? '+' + n : String(n)
 
 export const removeSubstringFromStringWithComas = (str: string, substr: string) => {
-    let result = str.replace(new RegExp(`,?\\s*${substr}`, 'i'), '')
-    let resultSplit = result.split(/,\s*/)
-
-    if (resultSplit.length === 2 && resultSplit[0] === '')
-        result = result.replace(/,\s*/, '')
-
-    return result
+    return str
+        .split(/,\s*/)
+        .filter(value => value.toLowerCase().trim() !== substr.toLowerCase())
+        .join(', ')
 }
